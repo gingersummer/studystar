f4ab2527933e981b5f68bc56f6fe401321b84abf
Your Name <your-email@example.com>
2026-07-09 12:07:16 -0500

    moved location of firebase variable

--- studyStar/src/environments/environment.prod.ts
+++ studyStar/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: { 
+    projectId: "studystar-flashcards", 
+    appId: "1:441722557953:web:e0f494bf0897f111f01200", 
+    storageBucket: "studystar-flashcards.firebasestorage.app", 
+    apiKey: "AIzaSyCP3TyMw20vB6Cy3RqNQN2dwbBonAU9sg0", 
+    authDomain: "studystar-flashcards.firebaseapp.com", 
+    messagingSenderId: "441722557953" 
+  }
 };
