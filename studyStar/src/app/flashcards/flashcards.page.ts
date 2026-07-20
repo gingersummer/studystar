cbab7a6c30c6fcbd9ce255cabab76d0620e76ac8
carsondeprez <carsondeprez@gmail.com>
2026-07-20 12:21:19 -0500

    hid_for_30_years

--- studyStar/src/app/flashcards/flashcards.page.ts
+++ studyStar/src/app/flashcards/flashcards.page.ts
@@ -70,6 +70,11 @@ export class FlashcardsPage implements OnInit {
     this.newSetName = ''
   }
 
+  redirectToDashboard() {
+    this.router.navigate(['/dashboard'])
+    this.menuCtrl.close('flashcards')
+  }
+
 
   ngOnInit() {
   }
