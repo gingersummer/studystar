62346ac517f0b3aab964297a97c7e85a4ab8c7b9
reillyforsyth <reillyforsyth@gmail.com>
2026-07-21 10:07:24 -0500

    src/app/flashcards/flashcards.page.ts

--- studyStar/src/app/flashcards/flashcards.page.ts
+++ studyStar/src/app/flashcards/flashcards.page.ts
@@ -123,15 +123,16 @@ export class FlashcardsPage implements OnInit {
     this.menuCtrl.close('collection')
     console.log('waht the sigma')
     this.newSetName = ''
+  }
   // redirectToDashboard() {
   //   this.router.navigate(['/dashboard'])
   //   this.menuCtrl.close('flashcards') 
   // }
 
-  createNewBlankCard(){
-
-  }
+ createNewBlankCard(){}
 
   
+  }
 
-}}
+
+
