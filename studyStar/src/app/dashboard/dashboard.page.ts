68717d873688d66bedbdc2d10a2e016805e1e3d5
carsondeprez <carsondeprez@gmail.com>
2026-07-23 12:02:53 -0500

    please

--- studyStar/src/app/dashboard/dashboard.page.ts
+++ studyStar/src/app/dashboard/dashboard.page.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
 import { Set } from '../models/Set';
 import { Flashcardsets } from '../services/FlashCardSets/flashcardsets';
 import { StreakService } from '../services/streak/streak-service';
 import { Alert } from '../services/alert';
+import { FlashcardcollectionComponent } from '../components/flashcardcollection/flashcardcollection.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,11 +15,12 @@ import { Alert } from '../services/alert';
 })
 export class DashboardPage implements OnInit {
 
+  @Input({required: true}) flashcardToDisplay!: Set
+  
   today = new Date();
   set: Set = new Set('', false, '', [], '');
   arrayOfSets: Set[] = []
   streakCount: number = 1;
-  
 
   constructor(
     private router: Router, 
